4cbc1279a1a6e6dd906c9838c85003e3f6e153b4
Qinux <qdaman90@gmail.com>
2022-03-18 14:50:09 -0500

    Added the game function also  made changes to playRound and added comments

--- script.js
+++ script.js
@@ -1,48 +1,90 @@
+let playerScore = 0;
+let cpuScore = 0;
+let draw = 0;
+let commentator = "";
+
+
+// ===================  This function is how the CPU comes up with its choice  ==============================
+
 let computerPlay = () => {
   // The numbers 1,2,3 will correspond with Rock Paper Scissor
   let num = Math.floor(Math.random() * 3);
 
   switch (num) {
     case 0:
-      console.log("Rock");
+      return "Rock";
       break;
     case 1:
-      console.log("Paper");
+      return "Paper";
       break;
     case 2:
-      console.log("Scissor");
+      return "Scissor";
       break;
     default:
       console.log("Something went wrong Brrzzttt");
   }
 };
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-    
+// ==============        This function takes in CPU & Players input, determines who wins and keeps score     =================
+
+let playRound = (playerSelection, computerSelection) => {
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+
   if (playerSelection === computerSelection) {
-    console.log("Tie!!");
+    commentator = "Tie";
+    draw++;
   } else if (playerSelection === "rock" && computerSelection === "scissor") {
-    console.log("You Win!!, Rock smashes Scissor");
+    commentator = "You Win!! Rock smashes Scissor";
+    playerScore++;
   } else if (playerSelection === "rock" && computerSelection === "paper") {
-    console.log("You Lose... Paper Covers Rock");
+    commentator = "You Lose.. Paper Covers Rock";
+    cpuScore++;
   } else if (playerSelection === "scissor" && computerSelection === "paper") {
-    console.log("You Win!!... Scissor Shreds Paper");
+    commentator = "You Win!! Scissor Shreds Paper";
+    playerScore++;
   } else if (playerSelection === "scissor" && computerSelection === "rock") {
-    console.log("You Lose.. Scissor Gets Smashed by Rock");
+    commentator = "You Lose.. Scissor Gets Smashed by Rock";
+    cpuScore++;
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-    console.log("You Win!! Paper Covers Rock");
+    commentator = "You Win!! Paper Covers Rock";
+    playerScore++;
   } else if (playerSelection === "paper" && computerSelection === "scissor") {
-    console.log("You Lose.. Paper Gets Shreded by Scissor");
-  } else if (playerSelection === "paper" && computerSelection === "scissor") {
-    console.log("You Lose.. Paper Gets Shreded by Scissor");
+    commentator = "You Lose.. Paper Gets Shreded by Scissor";
+    cpuScore++;
   } else {
-    console.log("Please Type Rock, Paper, or Scissor");
+    commentator = "Please Type Rock, Paper, or Scissor";
   }
-}
+  console.log(
+    "Score: " +
+      "You " +
+      playerScore +
+      " Computer " +
+      cpuScore +
+      " Draws " +
+      draw
+  );
+};
 
-  const playerSelection = "rock";
-  const computerSelection = computerPlay();
-  console.log(playRound(playerSelection, computerSelection));
+// =============  This function allows the game to be played 5 times then reports the winner ==========================
 
+let game = () => {
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Rock, Paper or Scissor?");
+    let computerSelection = computerPlay();
+
+    playRound(playerSelection, computerSelection);
+  }
+
+  if (playerScore > cpuScore) {
+    console.log("You Win!!");
+  } else if (playerScore < cpuScore) {
+    console.log("You Lose....");
+  } else {
+    console.log("Draw");
+  }
+};
+
+// =========================== Initiates the game  ================================
+
+game();
